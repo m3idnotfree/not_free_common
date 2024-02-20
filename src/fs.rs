ea95398c4ec64630a03d8fae604d04e4c81f73f3
m3id <johndoe@example.com>
2024-02-21 01:30:12 +0900

    fix(fs): PathBuf -> &PathBuf

--- src/fs.rs
+++ src/fs.rs
@@ -9,10 +9,10 @@ use tokio::{
 use crate::err::{ReadJsonError, WriteJsonError};
 
 pub async fn write_json<T: serde::Serialize>(
-    path: PathBuf,
+    path: &PathBuf,
     content: T,
 ) -> Result<(), WriteJsonError> {
-    let file = File::create(&path).await?;
+    let file = File::create(path).await?;
     let mut writer = BufWriter::new(file);
     let res = serde_json::to_string_pretty(&content)?;
     writer.write_all(res.as_bytes()).await?;
@@ -21,8 +21,8 @@ pub async fn write_json<T: serde::Serialize>(
     Ok(())
 }
 
-pub async fn read_json<T: de::DeserializeOwned>(path: PathBuf) -> Result<T, ReadJsonError> {
-    let file = fs::read_to_string(&path).await?;
+pub async fn read_json<T: de::DeserializeOwned>(path: &PathBuf) -> Result<T, ReadJsonError> {
+    let file = fs::read_to_string(path).await?;
     let result: T = serde_json::from_str(&file)?;
 
     Ok(result)
